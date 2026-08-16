46e3ebd7631b440ec020218f9a0ab77683b5e072
baracenalilljohn123-source <baracenalilljohn123@gmail.com>
2026-08-16 16:06:05 +0800

    Add files via upload

---
+++ check.php
@@ -0,0 +1,27 @@
+<?php
+
+$host ="localhost";
+$user ="root";
+$password ="";
+$databaseName = "bsis3b";
+
+$connection = new mysqli($host, $user, $password, $databaseName);
+
+$query ="SELECT * FROM customer";
+$result = mysqli_query($connection, $query);
+
+$username = $_POST['username'];
+$password =$_POST['password'];
+
+while ($row = mysqli_fetch_array($result)) {
+
+    if ($username == $row['username'] && $password == $row['PASSWORD']) {
+        echo "<script>alert('Correct')</script>";
+        header('location: home.php');
+    } else {
+        echo "<script>alert('Incorrect')</script>";
+    }
+
+}
+Compose
+Write to Edmon
---
+++ index.php
@@ -0,0 +1,33 @@
+!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <form action="pakicheck.php" method="post">
+            <hi>LOGIN FORM</hi>
+            <div class="input-box">
+                <input type="text" placeholder="Username" name="username">
+</div>
+<div class="input-box">
+    <input type="Password" placeholder="Password" name="password">
+    <i class="bx bxs-lock-alt'></i>
+</div>
+<div class="remember-forget">
+    <label> <input type="checkbox"> Remember Me</label>
+    <a href="#">Forget Password?</a>
+</div>
+<div>
+    <button>LOGIN</button>
+    <div class+"register-link">
+        <p>Dont have an account? <a href="#">Register</a></p>
+</div>
+</form>
+</div>
+</body>
+</html>
+Compose
